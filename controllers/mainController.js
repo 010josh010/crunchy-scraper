fcc75b4ab34ccd97df7224d38d6f3bef404d4998
Joshua Vaughn <Joshua_Vaughn@MacBook-Pro-4.local>
2017-06-02 09:51:34 -0400

    margin adjustments

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -14,7 +14,9 @@ const Comment = require('../models/Comment.js');
 
 /*Database configuration with mongoose begin----------*/
 const MONGODB_URI = 'mongodb://heroku_rq20k72c:lq7h15c1ius5rbq2457ldv5fm4@ds133368.mlab.com:33368/heroku_rq20k72c'; 
+
 mongoose.connect(MONGODB_URI);
+
 var db = mongoose.connection;
 //configuring mongoose to use native promises due to mpromise deprecation
 mongoose.promise = global.Promise; 
